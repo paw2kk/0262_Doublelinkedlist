d11bdde47160bf7013a05f395d8527f259210b3c
paw2kk <wndxakk@gmail.com>
2025-05-16 08:22:28 +0700

    membuat looping

--- doublywow.cpp
+++ doublywow.cpp
@@ -8,6 +8,71 @@ class Node{
     Node *next;
     Node *prev;
 };
+class DoubleLinkedList{
+    private:
+    Node *START;
+
+    public:
+    DoubleLinkedList(){
+        START = NULL;
+    }
+
+    void addNode(){
+        void addNode(){
+            int nim;
+            cout << "\nEnter the roll number of the student: ";
+            cin >> nim;
+
+            //Step 1 : Allocate memory for new node
+            Node *newNode = new Node();
+
+            //Step 2 : Assign value to the data fields
+            newNode->noMhs = nim;
+
+            //Step 3 : Insert at beginning if list is empty or nim is smallest
+            if (START == NULL || nim <= START->noMhs){
+                if (START != Null && nim == START->noMhs){
+                    cout << "\nDuplicate number not allowed" << endl;
+                    return;
+                }
+                //Step 4 : newNode.next = START
+                newNode-> = START;
+
+                //Step 5 : START.prev = newNode (if START exist)
+                if (START != NULL)
+                START->prev = newNode;
+
+                //Step 6 6: newNode.prev = NULL
+                newNode->prev = NULL;
+
+                //Step 7 : START = newNode
+                START = newNode;
+                return;
+            }
+                //Insert in between node
+                //Step 8 : Locate position for insertion
+                Node *current = START;
+                while (current->next != NULL && current->next->noMhs < nim)
+                {
+                    current = current->next;
+                }
+                if (current->next != NULL && nim == current->next-.noMhs)
+                {
+                    cout << "\nDuplicate roll numbers not allowed" << endl;
+                    return;
+                }
+                //Step 9 : Insert between current and current->next
+                newNode->next = current->next; //Step 9a : newNode.next = current.next
+                newNode->prev = current; // Step 9b : newNode.prev = current
+
+                //insert last node
+                if (current->next != NULL)
+                current-?next->prev = newNode; //Step 9c : current.next.prev = newNode
+
+                current->next = newNode; //Step 9d : current.next = newNode
+        }
+    }
+}
 
 int main(){
 
